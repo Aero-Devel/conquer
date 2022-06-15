a75417aa9b83ceff9e766b9546f5b3787d404f89
Jonathan Sargent <jmsargent94@gmail.com>
2022-06-15 08:30:17 +0200

    done adding fonts

--- frontend/App.js
+++ frontend/App.js
@@ -2,19 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 
+
+
 export default function App() {
 
+  /*
+  
+  Fonts 
+  
+   */
   const [loaded] = useFonts({
     Montserrat: require('./assets/fonts/Montserrat.ttf'),
+    SansitaBlack: require('./assets/fonts/Sansita-Black.ttf'),
+    SansitaIt: require('./assets/fonts/Sansita-Italic.ttf'),
+    SansitaReg: require('./assets/fonts/ScopeOne-Regular.ttf'),
+    Shrikhand: require('./assets/fonts/Shrikhand-Regular.ttf'),
+    ScopeOneReg: require('./assets/fonts/Shrikhand-Regular.ttf'),
   });
 
     if (!loaded) {
     return null;
   }
 
+
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Montserrat', fontSize: 44 }}>Vad händer</Text>
+      <Text style={{ fontFamily: 'SansitaBlack', fontSize: 44 }}>Vad händer</Text>
     </View>
   );
 }
